b34a1a9ff5473760302cc3a23f00f5eb1a60fd28
sbyers301 <44474840+sbyers301@users.noreply.github.com>
2021-04-12 23:18:14 -0400

    Add files via upload

---
+++ lab11.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+def words():
+	word1 = "Stapler before dawn"
+	word2 = "Waiting in perfect silence"
+	word3 = "Dreams of loose paper"
+
+	poem = word1 + "\n" + word2 + "\n" + word3
+
+	print poem
+
+if __name__ == '__main__':
+	words()
